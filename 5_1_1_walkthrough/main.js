e7866efa811c01673ed4a589295a821e42fc297d
bengrunfeld <benjamin.grunfeld@gmail.com>
2015-11-21 21:47:34 -0700

    Add examples

--- 5_1_1_walkthrough/main.js
+++ 5_1_1_walkthrough/main.js
@@ -8,27 +8,13 @@ var LibraryCollection = Backbone.Collection.extend({
   model: BookModel
 });
 
-
 // Instantiate the Collection
 var library = new LibraryCollection();
-
-
-
-
-
-
 
 // Create some Models
 var harry_potter = new BookModel({title: "Harry Potter"});
 var the_alchemist = new BookModel({title: "The Alchemist"});
 var grapes_of_wrath = new BookModel({title: "Grapes of Wrath"});
-
-
-
-
-
-
-
 
 // Add the models to the Collection
 library.add([harry_potter, the_alchemist, grapes_of_wrath]);
@@ -38,13 +24,6 @@ console.log("Add to Collection");
 console.log(library.length);
 console.log(JSON.stringify(library.toJSON()));
 
-
-
-
-
-
-
-
 // Remove a model from the collection
 library.remove(the_alchemist);
 
@@ -52,14 +31,3 @@ library.remove(the_alchemist);
 console.log("Remove from Collection");
 console.log(library.length);
 console.log(JSON.stringify(library.toJSON()));
-
-
-
-
-
-
-
-
-
-
-
