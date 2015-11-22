d2c2f11766e2e1842836bd883c54f8961950be12
bengrunfeld <benjamin.grunfeld@gmail.com>
2015-11-21 18:07:28 -0700

    Add new examples

---
+++ 4_5_walkthrough/main.js
@@ -0,0 +1,26 @@
+var ButtonView = Backbone.View.extend({  
+  tagName: 'button',
+  className: 'coolness',
+
+  initialize: function(attrs) {
+    this.options = attrs;
+  },
+
+  render: function(){
+    this.$el.html(this.options.args);
+    return this;
+  }
+});
+
+var ContainerView = Backbone.View.extend({
+  el: $('.target'),
+
+  render: function(){
+    var button_view = new ButtonView({args: "hello"});
+    this.$el.append(button_view.render().el);
+  }
+});
+
+
+var container_view = new ContainerView();
+container_view.render();
