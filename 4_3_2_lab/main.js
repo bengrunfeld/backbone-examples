480758f8373334e3e672f0ccfda1e48cacfc1d08
bengrunfeld <benjamin.grunfeld@gmail.com>
2015-12-05 15:46:25 -0700

    Bugfix: lab 4_3_2

--- 4_3_2_lab/main.js
+++ 4_3_2_lab/main.js
@@ -11,9 +11,8 @@ var ItemView = Backbone.View.extend({
   template: _.template($('#item-tpl').html()),
 
   render: function(){
-    
-    for (var i = 0; i < this.model.length){
-      var item_tpl = this.template(this.model.toJSON());
+    for (var i = 0; i < this.model.length; i++){
+      var item_tpl = this.template(this.model[i].toJSON());
       this.$el.append(item_tpl);
     }
     return this;
@@ -24,7 +23,7 @@ var broccoli = new ItemModel({img_src: 'broccoli.jpg', img_alt: 'broccoli', capt
 var steak = new ItemModel({img_src: 'steak.png', img_alt: 'steak', caption: 'Juicy Steak'});
 var tofu = new ItemModel({img_src: 'tofu.jpg', img_alt: 'tofu', caption: 'Seared Tofu'});
 
-broccoli_view = new ItemView({models: [broccoli, steak, tofu]});
+broccoli_view = new ItemView({model: [steak, broccoli, tofu]});
 
 broccoli_view.render();
 
